59fd36ceffbe8ee331367fa5797e99ed6e279041
Emmanuel John Templo <ejtemplo18@gmail.com>
2026-05-05 14:50:44 -0700

    Wrote Program Inputs
    Created the 3 program inputs as well as an if, elif and else statement for the premium plan.

--- data_bill.py
+++ data_bill.py
@@ -6,5 +6,15 @@ REGULAR_USER_OVERAGE_RATE_TIER_2 = 2
 PREMIUM_USER_OVERAGE_RATE_TIER_3 = 2
 REGULAR_USER_OVERAGE_RATE_TIER_3 = 3
 
-
 # Your code goes here:
+
+data_used = float(input("Enter data used (GB): "))
+monthly_plan_cost = float(input("Enter monthly plan cost: "))
+premium_plan = input("Do you have a premium plan? Enter yes or no: ")
+
+if premium_plan == "yes":
+    print("You have a premium plan.")
+elif premium_plan == "no":
+    print("You don't have a premium plan.")
+else:
+    print("Invalid input.")
